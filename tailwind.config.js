1663a1c1ff80dc520c27414671bccea822675b64
Vegan Speak <veganspeak@tuta.io>
2023-01-04 16:28:33 +0000

    Cleanup the mess

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
 	theme: {
 		extend: {
 			fontFamily: {
-				sans: ["Space Grotesk", "Rubik", ...defaultTheme.fontFamily.sans],
+				sans: ["Space Grotesk",, ...defaultTheme.fontFamily.sans],
 			},
       typography: ({ theme }) => ({
         neutral: {
